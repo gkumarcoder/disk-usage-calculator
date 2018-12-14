1b787f24efc4e0bdc5f2e6a63c32cd9cec90c871
Gaurav Kumar <gkumar4@outlook.com>
2018-12-14 23:57:24 +0530

    Update diskusage.py

--- diskusage.py
+++ diskusage.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
+
 import os
 import sys
 
-
 files = os.listdir(sys.argv[1])
+
 for x in files:
   fullpath = sys.argv[1] + "/" + x
   hrfilesize = os.path.getsize(fullpath) / 1024
