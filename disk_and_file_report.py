9ab8c430be11d61542e530da311de294d0243abb
Gaurav Kumar <gkumar4@outlook.com>
2025-08-11 08:55:08 +0530

    Create disk_and_file_report.py

---
+++ disk_and_file_report.py
@@ -0,0 +1,17 @@
+import sys 
+ 
+files = os.listdir(sys.argv[1])
+for x in files:
+  fullpath = sys.argv[1] + "/" + x
+  hrfilesize = os.path.getsize(fullpath) / 1024
+  
+  print (fullpath,hrfilesize) 
+  
+ import subprocess
+ threshold = 10
+ child = subprocess.Popen(['df', '-h'], stdout=subprocess.PIPE)
+ output = child.communicate()[0].strip().split("\n")
+ for x in output[1:]:
+            if int(x.split()[-2][:-1]) >= threshold:
+    
+            print x
